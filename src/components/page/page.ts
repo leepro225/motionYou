c76de18826c196e31ee55a37713b909fcf35d08b
lee pro <leepro225@gmail.com>
2021-09-21 00:13:25 +0900

    page component

---
+++ src/components/page/page.ts
@@ -0,0 +1,7 @@
+import { BaseComponent } from "../compoent";
+
+export class PageComponent extends BaseComponent<HTMLUListElement> {
+    constructor() {
+        super('<ul class="page">This is PageComponent!</ul>')
+    }
+}
