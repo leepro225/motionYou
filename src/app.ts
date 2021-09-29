f4f44f213bb133244612f55c1aec8573a5d49b59
lee pro <leepro225@gmail.com>
2021-09-29 23:37:57 +0900

    팝업 기능 추가

--- src/app.ts
+++ src/app.ts
@@ -2,25 +2,26 @@ import { ImageComponent } from "./components/page/item/image.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { VideoComponent } from "./components/page/item/video.js";
-import { PageComponent } from "./components/page/page.js";
+import { PageComponent, Composable } from "./components/page/page.js";
+import { Component } from "./components/compoent.js";
 
 class App {
-    private readonly page: PageComponent;
+    private readonly page: Component & Composable;
     constructor(appRoot: HTMLElement) {
         this.page = new PageComponent();
         this.page.attachTo(appRoot);
 
         const video = new VideoComponent('text', 'https://www.youtube.com/watch?v=7EcbNurbwKg');
-        video.attachTo(appRoot, 'beforeend');
+        this.page.addChild(video);
 
         const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
-        image.attachTo(appRoot, 'beforeend');
+        this.page.addChild(image);
     
         const note = new NoteComponent('Note title', 'Note Body');
-        note.attachTo(appRoot, 'beforeend');
+        this.page.addChild(note);
 
         const todo = new TodoComponent('Todo title', 'input');
-        todo.attachTo(appRoot, 'beforeend');
+        this.page.addChild(todo);
     }
 }
 
