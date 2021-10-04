0181865c9ce79aa486dacb969ae0bd09fc50ddfe
lee pro <leepro225@gmail.com>
2021-10-04 23:36:47 +0900

    dropdown function

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,14 @@ import { ImageComponent } from "./components/page/item/image.js";
 import { NoteComponent } from "./components/page/item/note.js";
 import { TodoComponent } from "./components/page/item/todo.js";
 import { VideoComponent } from "./components/page/item/video.js";
-import { PageComponent, Composable } from "./components/page/page.js";
+import { PageComponent, Composable, PageItemComponent } from "./components/page/page.js";
 import { Component } from "./components/compoent.js";
+import { InputDialog } from "./components/dialog/dialog.js";
 
 class App {
     private readonly page: Component & Composable;
     constructor(appRoot: HTMLElement) {
-        this.page = new PageComponent();
+        this.page = new PageComponent(PageItemComponent);
         this.page.attachTo(appRoot);
 
         const video = new VideoComponent('text', 'https://www.youtube.com/watch?v=7EcbNurbwKg');
@@ -22,6 +23,20 @@ class App {
 
         const todo = new TodoComponent('Todo title', 'input');
         this.page.addChild(todo);
+
+        const imageBtn = document.querySelector('#new-image')! as HTMLButtonElement;
+        imageBtn.addEventListener('click', () => {
+            const dialog = new InputDialog();
+
+            dialog.setOnCloseListener(() => {
+                dialog.removeFrom(document.body);
+            });
+            dialog.setOnSubmitListener(() => {
+                dialog.removeFrom(document.body);
+            });
+
+            dialog.attachTo(document.body);
+        })
     }
 }
 
